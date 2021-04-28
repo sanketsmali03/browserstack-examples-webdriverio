2a6df0f06cc686cefaad1f1a525abe5155eeaac6
roy-de-kleijn <79195396+roy-de-kleijn@users.noreply.github.com>
2021-04-28 12:50:18 +0200

    r1_review_patches (#11)
    Internal TU team reviews.
    Co-authored-by: Swapn Sharma <swapn@browserstack.com>
    Co-authored-by: Roy de Kleijn <dekleijn.roy@gmail.com>

--- resources/conf/wdio-docker-service.conf.js
+++ resources/conf/wdio-docker-service.conf.js
@@ -4,7 +4,17 @@ var _ = require("lodash");
 var overrides = {
   specs: [
     './src/test/suites/e2e/e2e.spec.js'
-  ]
+  ],
+  services: ['docker'],
+  dockerOptions: {
+    image: 'selenium/standalone-chrome',
+    healthCheck: 'http://localhost:4444',
+    startDelay: 2000,
+    options: {
+      p: ['4444:4444'],
+      shmSize: '2g'
+    }
+  },
 };
 
 exports.config = _.defaultsDeep(overrides, defaults.config);
